3990d09795aa3bb7268c3eaaba86765fe05e16dc
Sam Nicholls <sam.n@studio8media.co.uk>
2016-01-18 17:27:35 +0000

    samtools autoconf deps

--- samtools.sh
+++ samtools.sh
@@ -1,6 +1,7 @@
 cd $1
-mkdir ../autoconf-archive
-wget -O ../autoconf-archive/ax_with_htslib.m4 https://github.com/samtools/samtools/files/62424/ax_with_htslib.m4.txt
-aclocal -I.../autoconf-archive/m4   # Cache configure subroutines
+cd ../
+wget -O autoconf-archive/m4/ax_with_htslib.m4 https://github.com/samtools/samtools/files/62424/ax_with_htslib.m4.txt
+cd $1
+aclocal -I../autoconf-archive/m4   # Cache configure subroutines
 autoreconf -fvi       # Generate the configure script, if needed
 ./configure # Optional, needed for choosing optional functionality
